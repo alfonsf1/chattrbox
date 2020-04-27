170b8e407ec708bea8a91cea448eacfd0df5cb40
Alfonso Figueroa <figueroa.a@csu.fullerton.edu>
2020-04-26 22:37:32 -0700

    adding updates for hw 7

--- app/scripts/src/ws-client.js
+++ app/scripts/src/ws-client.js
@@ -6,7 +6,7 @@ function init(url) {
 }
 
 function registerOpenHandler(handlerFunction) {
-  socket.onopen = () => {
+  socket.onopen = function() {
     console.log('open');
     handlerFunction();
   };
